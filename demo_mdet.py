88fca8878fdeb61e9756e52fbb90501ba4375bcf
huilin66 <23039356r@connect.polyu.hk>
2024-08-26 16:44:30 +0800

    v2.8.2 update demo script

--- demo_mdet.py
+++ demo_mdet.py
@@ -1,80 +1,98 @@
 import torch
 from ultralytics import YOLO
-BATCH_SIZE = 32
-EPOCHS = 500
+BATCH_SIZE = 2
+EPOCHS = 5
 IMGSZ = 640
-DEVICE = torch.device('cuda:1')
-DATA = "billboard_mdet5_10_0806m.yaml"
-# DATA = "billboard_mdet5_10.yaml"
+CONF = 0.5
+DEVICE = torch.device('cuda:0')
+# DATA = "billboard_mdet5_10_0806m.yaml"
+DATA = "billboard_mdet5_10.yaml"
+FREEZE_NUMS = {
+    'yolov8' : 22,
+    'yolov9e': 42,
+    'yolov9' : 22,
+    'yolov10': 23,
+}
 
-def myolo_train(cfg_path, pretrain_path, auto_optim=True):
+# region meta tools
+
+def myolo_train(cfg_path, pretrain_path, auto_optim=True, retrain=False, **kwargs):
     model = YOLO(cfg_path, task='mdetect')
     model.load(pretrain_path)
-    if auto_optim:
-        model.train(data=DATA, device=DEVICE,
-                    epochs=EPOCHS, imgsz=IMGSZ, val=True, batch=BATCH_SIZE, patience=EPOCHS)
-    else:
-        model.train(data=DATA, device=DEVICE,
-                    optimizer='AdamW', lr0=0.0001,
-                    epochs=EPOCHS, imgsz=IMGSZ, val=True, batch=BATCH_SIZE, patience=EPOCHS)
 
-def myolo8_x(cfg_path, auto_optim=True):
-    myolo_train(cfg_path, 'yolov8x.pt', auto_optim=auto_optim)
+    train_params = {
+        'data': DATA,
+        'device': DEVICE,
+        'epochs': EPOCHS,
+        'imgsz': IMGSZ,
+        'val': True,
+        'batch': BATCH_SIZE,
+        'patience': EPOCHS
+    }
 
-def myolo9_e(cfg_path, auto_optim=True):
-    myolo_train(cfg_path, 'yolov9e.pt', auto_optim=auto_optim)
+    if not auto_optim:
+        train_params.update({
+            'optimizer': 'AdamW',
+            'lr0': 0.0001
+        })
+    if retrain:
+        train_params.update(
+            {
+                'freeze':get_freeze_num(cfg_path),
+                'freeze_head':['.cv2', '.cv3'],
+                'freeze_bn':True,
+            }
+        )
 
-def myolo10_x(cfg_path, auto_optim=True):
-    myolo_train(cfg_path, 'yolov10x.pt', auto_optim=auto_optim)
+    train_params.update(kwargs)
+    model.train(**train_params)
 
-
-def model_val(weight_path):
-    model = YOLO(weight_path, task='mdetect')
+def model_val(network, weight_path):
+    model = network(weight_path, task='mdetect')
     model.val(data=DATA, device=DEVICE)
 
-
-def model_predict(weight_path, img_dir, conf=0.5):
-    model = YOLO(weight_path, task='mdetect')
+def model_predict(network, weight_path, img_dir):
+    model = network(weight_path, task='mdetect')
     model.predict(
         img_dir,
         save=True,
-        conf=conf,
+        conf=CONF,
         device=DEVICE,
+        imgsz=IMGSZ,
     )
 
+def model_export(network, weight_path):
+    model = network(weight_path, task='mdetect')
+    model.export(format='onnx')
+
+# endregion
+
+
+# region other tools
+
+def get_freeze_num(cfg_path):
+    for k,v in FREEZE_NUMS.items():
+        if k in cfg_path:
+            return v
+    print('freeze num error for cfg_path {}'.format(cfg_path))
+    return None
+
+# endregion
+
+
+# region run tools
+
+def myolo8x(cfg_path, weight_path='yolov8x.pt', auto_optim=True, retrain=False, **kwargs):
+    myolo_train(cfg_path, pretrain_path=weight_path, auto_optim=auto_optim, retrain=retrain, **kwargs)
+
+def myolo9e(cfg_path, weight_path='yolov9e.pt', auto_optim=True, retrain=False, **kwargs):
+    myolo_train(cfg_path, pretrain_path=weight_path, auto_optim=auto_optim, retrain=retrain, **kwargs)
+
+def myolo10x(cfg_path, weight_path='yolov10x.pt', auto_optim=True, retrain=False, **kwargs):
+    myolo_train(cfg_path, pretrain_path=weight_path, auto_optim=auto_optim, retrain=retrain, **kwargs)
+
+# endregion
 
 if __name__ == '__main__':
     pass
-    # myolo8_modifiy('yolov8x-mdetect10-n1-gat1.yaml')
-    # myolo8_modifiy('yolov8x-mdetect10-n1-gat2.yaml')
-    # myolo8_modifiy('yolov8x-mdetect-gat11.yaml')
-    # myolo8_modifiy('yolov8x-mdetect-gat12.yaml')
-    # myolo8_modifiy('yolov8x-mdetect-gat13.yaml')
-    # myolo8_modifiy('yolov8x-mdetect-gat11-n0.yaml')
-    # myolo8_modifiy('yolov8x-mdetect-gat11-n1.yaml')
-    # myolo8_modifiy('yolov8x-mdetect-gat22-n0.yaml')
-    # myolo8_modifiy('yolov8x-mdetect-gat22-n1.yaml')
-
-    # myolo8_modifiy('yolov8x-mdetect0.yaml')
-    # myolo8_modifiy('yolov8x-mdetect0-sephead.yaml')
-    # myolo8_modifiy('yolov8x-mdetect0-sppftf0.yaml')
-    # myolo8_modifiy('yolov8x-mdetect0-sppftf0-sephead.yaml')
-    # myolo8_modifiy('yolov8x-mdetect0-sppftf1.yaml')
-    # myolo8_modifiy('yolov8x-mdetect0-sppftf1-sephead.yaml')
-
-    # myolo9_e('yolov9e-mdetect0.yaml')
-    # myolo9_e('yolov9e-mdetect0.yaml', auto_optim=False)
-    # model_val(r'runs/mdetect/train92/weights/best.pt')
-    # model_val(r'runs/mdetect/train95/weights/best.pt')
-
-    # myolo9_e('yolov9e-mdetect0-sephead.yaml', auto_optim=False)
-    # myolo9_e('yolov9e-mdetect0-sppftf0.yaml', auto_optim=False)
-    # myolo9_e('yolov9e-mdetect0-sppftf0-sephead.yaml', auto_optim=False)
-    # myolo9_e('yolov9e-mdetect0-sppftf1.yaml', auto_optim=False)
-    # myolo9_e('yolov9e-mdetect0-sppftf1-sephead.yaml', auto_optim=False)
-
-    # myolo10_x('yolov10x-mdetect0-sppftf0-sephead-gat11.yaml', auto_optim=False)
-    # myolo10_x('yolov10x-mdetect0-sppftf0-sephead-gat12.yaml', auto_optim=False)
-    myolo10_x('yolov10x-mdetect0-sppftf0-sephead-gat13.yaml', auto_optim=False)
-
-    # model_val(r'runs/mdetect/train112/weights/best.pt')
+    myolo8x(r'yolov8x-mdetect.yaml')
