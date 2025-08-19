cf7bc4eb58b922928eec88e93c09a0b5c13c1499
huilin66 <23039356r@connect.polyu.hk>
2025-08-19 17:17:49 +0800

    update 0819

---
+++ demo_infer.py
@@ -0,0 +1,35 @@
+from os import name
+import demo_base
+import torch
+
+demo_base.TASK = 'segment'
+demo_base.EPOCHS = 500
+demo_base.IMGSZ = 960
+demo_base.BATCH_SIZE = 16
+demo_base.DEVICE = torch.device('cuda:0')
+demo_base.DATA = "fusedata2177_seg_c6_0718.yaml"
+
+if __name__ == '__main__':
+    pass
+
+    # demo_base.yolo8('yolov8x-seg.yaml', auto_optim=False)
+    # demo_base.yolo8('yolov8x-seg-dlka3res.yaml', auto_optim=False)
+
+    # demo_base.model_val(
+    #     r'runs/segment/fusedata1948_seg_c6_0715-[yolov8x-seg]5/weights/best.pt',
+    #     save_json=True
+    # )
+    # demo_base.model_val(
+    #     r'runs/segment/fusedata1948_seg_c5_0715-[yolov8x-seg-dlka3res]/weights/best.pt',
+    #     save_json=True
+    # )
+    # demo_base.model_predict(
+    #     r'runs/segment/fusedata1422_seg_c6_check0708-[yolov8x-seg-dlka3res]/weights/best.pt',
+    #     r'/localnvme/data/billboard/fused_data/data1422_seg_c6_check0708/val/images',
+    # )
+
+    demo_base.model_predict(
+        weight_path=r'E:\repository\ultralytics\key_results\yolov8x_seg_signboard\best.pt',
+        img_dir=r'Y:\ZHL\isds\PS\task0812\merge_dir',
+        name=r'Y:\ZHL\isds\PS\task0812\merge_dir_infer'
+    )
