3d47afd3486ab6067fc985ab9ae1b83f1aca4c4e
huilin66 <23039356r@connect.polyu.hk>
2026-07-10 09:53:52 +0800

    chore: add HMT dataset configs and update demo scripts
    added 3 new HMT dataset yaml configs: hmt_t_f_p01_merge.yaml, hmt_rgb_f_p01_merge.yaml and hmt_t_f_p01_merge_v2.yaml
    updated demo_det1.py to use v2 thermal merge dataset and commented out old training runs
    updated demo_det3.py confidence threshold and adjusted validation script calls

--- demo_det1.py
+++ demo_det1.py
@@ -4,7 +4,7 @@ import os
 
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 demo_base.TASK = 'detect'
-demo_base.EPOCHS = 300
+demo_base.EPOCHS = 100
 demo_base.IMGSZ = 640
 demo_base.DEVICE = torch.device('cuda:0')
 demo_base.BATCH_SIZE = 16
@@ -28,9 +28,50 @@ if __name__ == '__main__':
     # demo_base.yolo12('yolo12x.yaml', auto_optim=False, name=NAME, data="hmt_rgb_rgbt.yaml")
     # demo_base.yolo26('yolo26x.yaml', auto_optim=False, name=NAME, data="hmt_rgb_rgbt.yaml")
 
-    demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_bp_cube.yaml")
-    demo_base.yolo9('yolov9e.yaml', auto_optim=False, name=NAME, data="hmt_bp_cube.yaml")
-    demo_base.yolo10('yolov10x.yaml', auto_optim=False, name=NAME, data="hmt_bp_cube.yaml")
-    demo_base.yolo11('yolo11x.yaml', auto_optim=False, name=NAME, data="hmt_bp_cube.yaml")
-    demo_base.yolo12('yolo12x.yaml', auto_optim=False, name=NAME, data="hmt_bp_cube.yaml")
-    demo_base.yolo26('yolo26x.yaml', auto_optim=False, name=NAME, data="hmt_bp_cube.yaml")
+    # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_bp_cube_f_p01.yaml", imgsz=1280)
+    # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_rgb_f_p01.yaml", imgsz=1280)
+    # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_t_f_p01.yaml", imgsz=1280)
+
+    # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_bp_cube.yaml")
+    # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_rgb.yaml")
+    # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_t.yaml")
+    # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_bp_cube_f_p01.yaml")
+    # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_rgb_f_p01.yaml")
+    # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_t_f_p01.yaml")
+
+    # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_bp_cube.yaml", imgsz=960)
+    # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_rgb.yaml", imgsz=960)
+    # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_t.yaml", imgsz=960)
+    # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_bp_cube_f_p01.yaml", imgsz=960)
+    # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_rgb_f_p01.yaml", imgsz=960)
+    # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_t_f_p01.yaml", imgsz=960)
+    
+    # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_bp_cube.yaml", imgsz=1280)
+    # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_rgb.yaml", imgsz=1280)
+    # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_t.yaml", imgsz=1280)
+    # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_bp_cube_f_p01.yaml", imgsz=1280)
+    # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_rgb_f_p01.yaml", imgsz=1280)
+    # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_t_f_p01.yaml", imgsz=1280)
+
+    # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_bp_cube.yaml")
+    # demo_base.yolo9('yolov9e.yaml', auto_optim=False, name=NAME, data="hmt_bp_cube.yaml")
+    # demo_base.yolo10('yolov10x.yaml', auto_optim=False, name=NAME, data="hmt_bp_cube.yaml")
+    # demo_base.yolo11('yolo11x.yaml', auto_optim=False, name=NAME, data="hmt_bp_cube.yaml")
+    # demo_base.yolo12('yolo12x.yaml', auto_optim=False, name=NAME, data="hmt_bp_cube.yaml")
+    # demo_base.yolo26('yolo26x.yaml', auto_optim=False, name=NAME, data="hmt_bp_cube.yaml")
+    # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_rgb.yaml")
+    # demo_base.yolo9('yolov9e.yaml', auto_optim=False, name=NAME, data="hmt_rgb.yaml")
+    # demo_base.yolo10('yolov10x.yaml', auto_optim=False, name=NAME, data="hmt_rgb.yaml")
+    # demo_base.yolo11('yolo11x.yaml', auto_optim=False, name=NAME, data="hmt_rgb.yaml")
+    # demo_base.yolo12('yolo12x.yaml', auto_optim=False, name=NAME, data="hmt_rgb.yaml")
+    # demo_base.yolo26('yolo26x.yaml', auto_optim=False, name=NAME, data="hmt_rgb.yaml")
+    # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_t.yaml")
+    # demo_base.yolo9('yolov9e.yaml', auto_optim=False, name=NAME, data="hmt_t.yaml")
+    # demo_base.yolo10('yolov10x.yaml', auto_optim=False, name=NAME, data="hmt_t.yaml")
+    # demo_base.yolo11('yolo11x.yaml', auto_optim=False, name=NAME, data="hmt_t.yaml")
+    # demo_base.yolo12('yolo12x.yaml', auto_optim=False, name=NAME, data="hmt_t.yaml")
+    # demo_base.yolo26('yolo26x.yaml', auto_optim=False, name=NAME, data="hmt_t.yaml")
+
+    # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_t_f_p01_merge.yaml")
+    # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_rgb_f_p01_merge.yaml")
+    demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_t_f_p01_merge_v2.yaml", cls_pw=0.5)
--- demo_det3.py
+++ demo_det3.py
@@ -10,46 +10,76 @@ demo_base.DEVICE = torch.device('cuda:1')
 demo_base.BATCH_SIZE = 16
 # demo_base.DATA = ".yaml"
 # demo_base.CONF = 0.5
-demo_base.CONF_VAL = 0.5
+demo_base.CONF_VAL = 0.01
 
 if __name__ == '__main__':
     pass
     NAME = None
 
-    csv_path = r'summary_conf_0.5.csv'
+    csv_path = r'summary_conf_0.01.csv'
+    # demo_base.model_val('hmt_rgb_f_p01-[yolov8x]', save_conf=True, save_txt=True)
+    # demo_base.model_val('hmt_bp_cube_f_p01-[yolov8x]', save_conf=True, save_txt=True)
 
-    demo_base.model_val_summary('hmt_bp_cube-[yolov8x]', csv_path=csv_path)
-    demo_base.model_val_summary('hmt_bp_cube-[yolov9e]', csv_path=csv_path)
-    demo_base.model_val_summary('hmt_bp_cube-[yolov10x]', csv_path=csv_path)
-    demo_base.model_val_summary('hmt_bp_cube-[yolo11x]', csv_path=csv_path)
-    demo_base.model_val_summary('hmt_bp_cube-[yolo12x]', csv_path=csv_path)
-    demo_base.model_val_summary('hmt_bp_cube-[yolo26x]', csv_path=csv_path)
+    demo_base.model_val('hmt_t_f_p01_merge-[yolov8x]', save_conf=True, save_txt=True)
+    # demo_base.model_val('hmt_t_f_p01_merge-[yolov8x]', save_conf=True, save_txt=True)
+
+    # demo_base.model_val_summary('hmt_bp_cube-[yolov8x]2', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_bp_cube-[yolov8x]3', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_bp_cube-[yolov8x]4', csv_path=csv_path)
+
+    # demo_base.model_val_summary('hmt_bp_cube_f_p01-[yolov8x]', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_bp_cube_f_p01-[yolov8x]2', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_bp_cube_f_p01-[yolov8x]3', csv_path=csv_path)
 
 
-    demo_base.model_val_summary('hmt_t-[yolov8x]', csv_path=csv_path)
-    demo_base.model_val_summary('hmt_t-[yolov9e]', csv_path=csv_path)
-    demo_base.model_val_summary('hmt_t-[yolov10x]', csv_path=csv_path)
-    demo_base.model_val_summary('hmt_t-[yolo11x]', csv_path=csv_path)
-    demo_base.model_val_summary('hmt_t-[yolo12x]', csv_path=csv_path)
-    demo_base.model_val_summary('hmt_t-[yolo26x]', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_rgb-[yolov8x]2', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_rgb-[yolov8x]3', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_rgb-[yolov8x]4', csv_path=csv_path)
 
-    demo_base.model_val_summary('hmt_t_rgbt-[yolov8x]', csv_path=csv_path)
-    demo_base.model_val_summary('hmt_t_rgbt-[yolov9e]', csv_path=csv_path)
-    demo_base.model_val_summary('hmt_t_rgbt-[yolov10x]', csv_path=csv_path)
-    demo_base.model_val_summary('hmt_t_rgbt-[yolo11x]', csv_path=csv_path)
-    demo_base.model_val_summary('hmt_t_rgbt-[yolo12x]', csv_path=csv_path)
-    demo_base.model_val_summary('hmt_t_rgbt-[yolo26x]', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_rgb_f_p01-[yolov8x]', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_rgb_f_p01-[yolov8x]2', csv_path=csv_path)
 
-    demo_base.model_val_summary('hmt_rgb-[yolov8x]', csv_path=csv_path)
-    demo_base.model_val_summary('hmt_rgb-[yolov9e]', csv_path=csv_path)
-    demo_base.model_val_summary('hmt_rgb-[yolov10x]', csv_path=csv_path)
-    demo_base.model_val_summary('hmt_rgb-[yolo11x]', csv_path=csv_path)
-    demo_base.model_val_summary('hmt_rgb-[yolo12x]', csv_path=csv_path)
-    demo_base.model_val_summary('hmt_rgb-[yolo26x]', csv_path=csv_path)
 
-    demo_base.model_val_summary('hmt_rgb_rgbt-[yolov8x]', csv_path=csv_path)
-    demo_base.model_val_summary('hmt_rgb_rgbt-[yolov9e]', csv_path=csv_path)
-    demo_base.model_val_summary('hmt_rgb_rgbt-[yolov10x]', csv_path=csv_path)
-    demo_base.model_val_summary('hmt_rgb_rgbt-[yolo11x]', csv_path=csv_path)
-    demo_base.model_val_summary('hmt_rgb_rgbt-[yolo12x]', csv_path=csv_path)
-    demo_base.model_val_summary('hmt_rgb_rgbt-[yolo26x]', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_t-[yolov8x]2', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_t-[yolov8x]3', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_t-[yolov8x]4', csv_path=csv_path)
+
+    # demo_base.model_val_summary('hmt_t_f_p01-[yolov8x]', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_t_f_p01-[yolov8x]2', csv_path=csv_path)
+
+
+    # demo_base.model_val_summary('hmt_bp_cube-[yolov8x]', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_bp_cube-[yolov9e]', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_bp_cube-[yolov10x]', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_bp_cube-[yolo11x]', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_bp_cube-[yolo12x]', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_bp_cube-[yolo26x]', csv_path=csv_path)
+
+
+    # demo_base.model_val_summary('hmt_t-[yolov8x]', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_t-[yolov9e]', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_t-[yolov10x]', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_t-[yolo11x]', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_t-[yolo12x]', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_t-[yolo26x]', csv_path=csv_path)
+
+    # demo_base.model_val_summary('hmt_t_rgbt-[yolov8x]', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_t_rgbt-[yolov9e]', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_t_rgbt-[yolov10x]', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_t_rgbt-[yolo11x]', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_t_rgbt-[yolo12x]', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_t_rgbt-[yolo26x]', csv_path=csv_path)
+
+    # demo_base.model_val_summary('hmt_rgb-[yolov8x]', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_rgb-[yolov9e]', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_rgb-[yolov10x]', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_rgb-[yolo11x]', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_rgb-[yolo12x]', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_rgb-[yolo26x]', csv_path=csv_path)
+
+    # demo_base.model_val_summary('hmt_rgb_rgbt-[yolov8x]', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_rgb_rgbt-[yolov9e]', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_rgb_rgbt-[yolov10x]', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_rgb_rgbt-[yolo11x]', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_rgb_rgbt-[yolo12x]', csv_path=csv_path)
+    # demo_base.model_val_summary('hmt_rgb_rgbt-[yolo26x]', csv_path=csv_path)
