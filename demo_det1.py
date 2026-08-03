bb2673b72d7cb3bf4ba2414f3fee9736c888f3a1
huilin66 <23039356r@connect.polyu.hk>
2026-08-03 14:46:29 +0800

    feat: refactor demo scripts and add new dataset configurations

--- demo_det1.py
+++ demo_det1.py
@@ -1,20 +1,30 @@
-import demo_base
-import torch
 import os
 
-os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
-demo_base.TASK = 'detect'
+import torch
+
+import demo_base
+
+os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
+demo_base.TASK = "detect"
 demo_base.EPOCHS = 300
 demo_base.IMGSZ = 640
-demo_base.DEVICE = torch.device('cuda:0')
+demo_base.DEVICE = torch.device("cuda:1")
 demo_base.BATCH_SIZE = 16
-demo_base.DATA = "hmt_t.yaml"
+demo_base.DATA = "tf_det_1.yaml"
 # demo_base.CONF = 0.5
 
 
-if __name__ == '__main__':
-    pass
+if __name__ == "__main__":
     NAME = None
+    demo_base.model_train_resume(
+        r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/tf_det_1-[yolov8x]-2/weights/last.pt"
+    )
+    # demo_base.yolo8("yolov8x.yaml", auto_optim=False, name=NAME)
+    demo_base.yolo8("yolov8x.yaml", auto_optim=False, name=NAME, cls_pw=0.5)
+
+    demo_base.yolo9("yolov9e.yaml", auto_optim=False, name=NAME)
+    demo_base.yolo9("yolov9e.yaml", auto_optim=False, name=NAME, cls_pw=0.5)
+
     # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_rgb.yaml")
     # demo_base.yolo9('yolov9e.yaml', auto_optim=False, name=NAME, data="hmt_rgb.yaml")
     # demo_base.yolo10('yolov10x.yaml', auto_optim=False, name=NAME, data="hmt_rgb.yaml")
@@ -45,7 +55,7 @@ if __name__ == '__main__':
     # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_bp_cube_f_p01.yaml", imgsz=960)
     # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_rgb_f_p01.yaml", imgsz=960)
     # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_t_f_p01.yaml", imgsz=960)
-    
+
     # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_bp_cube.yaml", imgsz=1280)
     # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_rgb.yaml", imgsz=1280)
     # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_t.yaml", imgsz=1280)
@@ -78,18 +88,18 @@ if __name__ == '__main__':
 
     # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_rgb_merge_f01.yaml", cls_pw=0.5)
     # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_rgb_merge.yaml", cls_pw=0.5)
-    
-    demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_rgb_merge_v2.yaml", cls_pw=0.5)
-    demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_rgb_merge.yaml", cls_pw=0.5)
-    demo_base.yolo26('yolo26x.yaml', auto_optim=False, name=NAME, data="hmt_rgb_merge_v2.yaml", cls_pw=0.5)
-    demo_base.yolo26('yolo26x.yaml', auto_optim=False, name=NAME, data="hmt_rgb_merge.yaml", cls_pw=0.5)
 
-    demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_rgb_merge_v2.yaml", cls_pw=0.5, imgsz=960)
-    demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_rgb_merge.yaml", cls_pw=0.5, imgsz=960)
-    demo_base.yolo26('yolo26x.yaml', auto_optim=False, name=NAME, data="hmt_rgb_merge_v2.yaml", cls_pw=0.5, imgsz=960)
-    demo_base.yolo26('yolo26x.yaml', auto_optim=False, name=NAME, data="hmt_rgb_merge.yaml", cls_pw=0.5, imgsz=960)
+    # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_rgb_merge_v2.yaml", cls_pw=0.5)
+    # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_rgb_merge.yaml", cls_pw=0.5)
+    # demo_base.yolo26('yolo26x.yaml', auto_optim=False, name=NAME, data="hmt_rgb_merge_v2.yaml", cls_pw=0.5)
+    # demo_base.yolo26('yolo26x.yaml', auto_optim=False, name=NAME, data="hmt_rgb_merge.yaml", cls_pw=0.5)
 
-    demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_rgb_merge_v2.yaml", cls_pw=0.5, imgsz=1280)
-    demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_rgb_merge.yaml", cls_pw=0.5, imgsz=1280)
-    demo_base.yolo26('yolo26x.yaml', auto_optim=False, name=NAME, data="hmt_rgb_merge_v2.yaml", cls_pw=0.5, imgsz=1280)
-    demo_base.yolo26('yolo26x.yaml', auto_optim=False, name=NAME, data="hmt_rgb_merge.yaml", cls_pw=0.5, imgsz=1280)
+    # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_rgb_merge_v2.yaml", cls_pw=0.5, imgsz=960)
+    # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_rgb_merge.yaml", cls_pw=0.5, imgsz=960)
+    # demo_base.yolo26('yolo26x.yaml', auto_optim=False, name=NAME, data="hmt_rgb_merge_v2.yaml", cls_pw=0.5, imgsz=960)
+    # demo_base.yolo26('yolo26x.yaml', auto_optim=False, name=NAME, data="hmt_rgb_merge.yaml", cls_pw=0.5, imgsz=960)
+
+    # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_rgb_merge_v2.yaml", cls_pw=0.5, imgsz=1280)
+    # demo_base.yolo8('yolov8x.yaml', auto_optim=False, name=NAME, data="hmt_rgb_merge.yaml", cls_pw=0.5, imgsz=1280)
+    # demo_base.yolo26('yolo26x.yaml', auto_optim=False, name=NAME, data="hmt_rgb_merge_v2.yaml", cls_pw=0.5, imgsz=1280)
+    # demo_base.yolo26('yolo26x.yaml', auto_optim=False, name=NAME, data="hmt_rgb_merge.yaml", cls_pw=0.5, imgsz=1280)
--- demo_det3.py
+++ demo_det3.py
@@ -1,27 +1,66 @@
-import demo_base
-import torch
 import os
 
-os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
-demo_base.TASK = 'detect'
+import torch
+
+import demo_base
+
+os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
+demo_base.TASK = "detect"
 demo_base.EPOCHS = 300
 demo_base.IMGSZ = 640
-demo_base.DEVICE = torch.device('cuda:1')
+demo_base.DEVICE = torch.device("cuda:0")
 demo_base.BATCH_SIZE = 16
 # demo_base.DATA = ".yaml"
 # demo_base.CONF = 0.5
 demo_base.CONF_VAL = 0.01
 
-if __name__ == '__main__':
-    pass
+if __name__ == "__main__":
     NAME = None
 
-    csv_path = r'summary_conf_0.01.csv'
+    csv_path = r"summary_conf_0.01.csv"
+
+    # demo_base.model_val(
+    #     "/localnvme/project/aic_mdet/models/ultralytics/runs/detect/tf_det_1-[yolov8x]-2/weights/best.pt",
+    #     weight_name=False,
+    # )
+
+    demo_base.model_val_summary(
+        "/localnvme/project/aic_mdet/models/ultralytics/runs/detect/f_defect_1-[yolov8x]-3/weights/best.pt",
+        weight_name=False,
+    )
+    demo_base.model_val_summary(
+        "/localnvme/project/aic_mdet/models/ultralytics/runs/detect/f_defect_1-[yolov8x]-4/weights/best.pt",
+        weight_name=False,
+    )
+
+    demo_base.model_val(
+        "/localnvme/project/aic_mdet/models/ultralytics/runs/detect/f_defect_1-[yolov9e]/weights/best.pt",
+        weight_name=False,
+    )
+    demo_base.model_val(
+        "/localnvme/project/aic_mdet/models/ultralytics/runs/detect/f_defect_1-[yolov9e]-2/weights/best.pt",
+        weight_name=False,
+    )
+
+    # demo_base.model_val("tf_defect_3-[yolov8x]")
+    # demo_base.model_val("tf_defect_3-[yolov8x]-2")
+    # demo_base.model_val("tf_defect_3-[yolo26x]")
+    # demo_base.model_val("tf_defect_3-[yolo26x]-2")
+
+    # demo_base.model_val("hmt_t-[yolov8x]-6")
+    # demo_base.model_val("hmt_rgb_merge_v2-[yolov8x]-5")
+    # demo_base.model_val("hmt_bp_cube-[yolov8x]2")
+
     # demo_base.model_val('hmt_t-[yolov8x]-8', conf=0.001, iou=0.5)
     # demo_base.model_val('hmt_rgb_merge_f01-[yolov8x]')
     # demo_base.model_val('hmt_rgb_merge-[yolov8x]-2')
 
-    demo_base.model_val('hmt_rgb_merge-[yolov8x]-4', save_conf=True, save_txt=True)
+    # demo_base.model_val("tf_defect_3-[yolov8x]", save_conf=True, save_txt=True)
+    # demo_base.model_val("tf_defect_3-[yolov8x]-2")
+    # demo_base.model_val("tf_defect_3-[yolo26x]")
+    # demo_base.model_val("tf_defect_3-[yolo26x]-2")
+
+    # demo_base.model_val('hmt_rgb_merge-[yolov8x]-4', save_conf=True, save_txt=True)
     # demo_base.model_val('hmt_t-[yolov8x]-6', save_conf=True, save_txt=True)
     # demo_base.model_val('hmt_bp_cube_f_p01-[yolov8x]', save_conf=True, save_txt=True)
 
@@ -36,14 +75,12 @@ if __name__ == '__main__':
     # demo_base.model_val_summary('hmt_bp_cube_f_p01-[yolov8x]2', csv_path=csv_path)
     # demo_base.model_val_summary('hmt_bp_cube_f_p01-[yolov8x]3', csv_path=csv_path)
 
-
     # demo_base.model_val_summary('hmt_rgb-[yolov8x]2', csv_path=csv_path)
     # demo_base.model_val_summary('hmt_rgb-[yolov8x]3', csv_path=csv_path)
     # demo_base.model_val_summary('hmt_rgb-[yolov8x]4', csv_path=csv_path)
 
     # demo_base.model_val_summary('hmt_rgb_f_p01-[yolov8x]', csv_path=csv_path)
     # demo_base.model_val_summary('hmt_rgb_f_p01-[yolov8x]2', csv_path=csv_path)
-
 
     # demo_base.model_val_summary('hmt_t-[yolov8x]2', csv_path=csv_path)
     # demo_base.model_val_summary('hmt_t-[yolov8x]3', csv_path=csv_path)
@@ -52,14 +89,12 @@ if __name__ == '__main__':
     # demo_base.model_val_summary('hmt_t_f_p01-[yolov8x]', csv_path=csv_path)
     # demo_base.model_val_summary('hmt_t_f_p01-[yolov8x]2', csv_path=csv_path)
 
-
     # demo_base.model_val_summary('hmt_bp_cube-[yolov8x]', csv_path=csv_path)
     # demo_base.model_val_summary('hmt_bp_cube-[yolov9e]', csv_path=csv_path)
     # demo_base.model_val_summary('hmt_bp_cube-[yolov10x]', csv_path=csv_path)
     # demo_base.model_val_summary('hmt_bp_cube-[yolo11x]', csv_path=csv_path)
     # demo_base.model_val_summary('hmt_bp_cube-[yolo12x]', csv_path=csv_path)
     # demo_base.model_val_summary('hmt_bp_cube-[yolo26x]', csv_path=csv_path)
-
 
     # demo_base.model_val_summary('hmt_t-[yolov8x]', csv_path=csv_path)
     # demo_base.model_val_summary('hmt_t-[yolov9e]', csv_path=csv_path)
