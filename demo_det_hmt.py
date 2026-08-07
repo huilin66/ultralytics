477d71171c860f5e727e5bef4ec906e58a297451
huilin66 <23039356r@connect.polyu.hk>
2026-08-08 01:12:58 +0800

    feat: add YOLO model invocations for various datasets in demo_det_hmt.py

--- demo_det_hmt.py
+++ demo_det_hmt.py
@@ -6,7 +6,7 @@ import demo_base
 
 os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
 demo_base.TASK = "detect"
-demo_base.EPOCHS = 200
+demo_base.EPOCHS = 300
 demo_base.IMGSZ = 640
 demo_base.DEVICE = torch.device("cuda:1")
 demo_base.BATCH_SIZE = 16
@@ -199,3 +199,24 @@ if __name__ == "__main__":
     #     save_conf=True,
     #     conf=0.001,
     # )
+
+    demo_base.yolo8("yolov8x.yaml", auto_optim=False, name=NAME, data="hmt_t.yaml")
+    demo_base.yolo9("yolov9e.yaml", auto_optim=False, name=NAME, data="hmt_t.yaml")
+    demo_base.yolo10("yolov10x.yaml", auto_optim=False, name=NAME, data="hmt_t.yaml")
+    demo_base.yolo11("yolo11x.yaml", auto_optim=False, name=NAME, data="hmt_t.yaml")
+    demo_base.yolo12("yolo12x.yaml", auto_optim=False, name=NAME, data="hmt_t.yaml")
+    demo_base.yolo26("yolo26x.yaml", auto_optim=False, name=NAME, data="hmt_t.yaml")
+
+    demo_base.yolo8("yolov8x.yaml", auto_optim=False, name=NAME, data="hmt_rgb.yaml")
+    demo_base.yolo9("yolov9e.yaml", auto_optim=False, name=NAME, data="hmt_rgb.yaml")
+    demo_base.yolo10("yolov10x.yaml", auto_optim=False, name=NAME, data="hmt_rgb.yaml")
+    demo_base.yolo11("yolo11x.yaml", auto_optim=False, name=NAME, data="hmt_rgb.yaml")
+    demo_base.yolo12("yolo12x.yaml", auto_optim=False, name=NAME, data="hmt_rgb.yaml")
+    demo_base.yolo26("yolo26x.yaml", auto_optim=False, name=NAME, data="hmt_rgb.yaml")
+
+    demo_base.yolo8("yolov8x.yaml", auto_optim=False, name=NAME, data="hmt_bp_cube.yaml")
+    demo_base.yolo9("yolov9e.yaml", auto_optim=False, name=NAME, data="hmthmt_bp_cube_rgb.yaml")
+    demo_base.yolo10("yolov10x.yaml", auto_optim=False, name=NAME, data="hmt_bp_cube.yaml")
+    demo_base.yolo11("yolo11x.yaml", auto_optim=False, name=NAME, data="hmt_bp_cube.yaml")
+    demo_base.yolo12("yolo12x.yaml", auto_optim=False, name=NAME, data="hmt_bp_cube.yaml")
+    demo_base.yolo26("yolo26x.yaml", auto_optim=False, name=NAME, data="hmt_bp_cube.yaml")
