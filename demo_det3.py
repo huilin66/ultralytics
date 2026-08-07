db00db65ac6ad85f7a9a91a6845198d9aec88650
huilin66 <23039356r@connect.polyu.hk>
2026-08-08 01:08:40 +0800

    fix: update dataset paths and class names in HMT configuration files

--- demo_det3.py
+++ demo_det3.py
@@ -6,120 +6,16 @@ import demo_base
 
 os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
 demo_base.TASK = "detect"
-demo_base.EPOCHS = 300
+demo_base.EPOCHS = 200
 demo_base.IMGSZ = 640
 demo_base.DEVICE = torch.device("cuda:0")
 demo_base.BATCH_SIZE = 16
 # demo_base.DATA = ".yaml"
 # demo_base.CONF = 0.5
-demo_base.CONF_VAL = 0.01
+demo_base.CONF_VAL = 0.001
 
 if __name__ == "__main__":
-    NAME = None
+    NAME = "debug"
 
     csv_path = r"summary_conf_0.01.csv"
-
-    # demo_base.model_val(
-    #     "/localnvme/project/aic_mdet/models/ultralytics/runs/detect/tf_det_1-[yolov8x]-2/weights/best.pt",
-    #     weight_name=False,
-    # )
-
-    demo_base.model_val_summary(
-        "/localnvme/project/aic_mdet/models/ultralytics/runs/detect/f_defect_1-[yolov8x]-3/weights/best.pt",
-        weight_name=False,
-    )
-    demo_base.model_val_summary(
-        "/localnvme/project/aic_mdet/models/ultralytics/runs/detect/f_defect_1-[yolov8x]-4/weights/best.pt",
-        weight_name=False,
-    )
-
-    demo_base.model_val(
-        "/localnvme/project/aic_mdet/models/ultralytics/runs/detect/f_defect_1-[yolov9e]/weights/best.pt",
-        weight_name=False,
-    )
-    demo_base.model_val(
-        "/localnvme/project/aic_mdet/models/ultralytics/runs/detect/f_defect_1-[yolov9e]-2/weights/best.pt",
-        weight_name=False,
-    )
-
-    # demo_base.model_val("tf_defect_3-[yolov8x]")
-    # demo_base.model_val("tf_defect_3-[yolov8x]-2")
-    # demo_base.model_val("tf_defect_3-[yolo26x]")
-    # demo_base.model_val("tf_defect_3-[yolo26x]-2")
-
-    # demo_base.model_val("hmt_t-[yolov8x]-6")
-    # demo_base.model_val("hmt_rgb_merge_v2-[yolov8x]-5")
-    # demo_base.model_val("hmt_bp_cube-[yolov8x]2")
-
-    # demo_base.model_val('hmt_t-[yolov8x]-8', conf=0.001, iou=0.5)
-    # demo_base.model_val('hmt_rgb_merge_f01-[yolov8x]')
-    # demo_base.model_val('hmt_rgb_merge-[yolov8x]-2')
-
-    # demo_base.model_val("tf_defect_3-[yolov8x]", save_conf=True, save_txt=True)
-    # demo_base.model_val("tf_defect_3-[yolov8x]-2")
-    # demo_base.model_val("tf_defect_3-[yolo26x]")
-    # demo_base.model_val("tf_defect_3-[yolo26x]-2")
-
-    # demo_base.model_val('hmt_rgb_merge-[yolov8x]-4', save_conf=True, save_txt=True)
-    # demo_base.model_val('hmt_t-[yolov8x]-6', save_conf=True, save_txt=True)
-    # demo_base.model_val('hmt_bp_cube_f_p01-[yolov8x]', save_conf=True, save_txt=True)
-
-    # demo_base.model_val('hmt_t_f_p01_merge-[yolov8x]', save_conf=True, save_txt=True)
-    # demo_base.model_val('hmt_t_f_p01_merge-[yolov8x]', save_conf=True, save_txt=True)
-
-    # demo_base.model_val_summary('hmt_bp_cube-[yolov8x]2', csv_path=csv_path)
-    # demo_base.model_val_summary('hmt_bp_cube-[yolov8x]3', csv_path=csv_path)
-    # demo_base.model_val_summary('hmt_bp_cube-[yolov8x]4', csv_path=csv_path)
-
-    # demo_base.model_val_summary('hmt_bp_cube_f_p01-[yolov8x]', csv_path=csv_path)
-    # demo_base.model_val_summary('hmt_bp_cube_f_p01-[yolov8x]2', csv_path=csv_path)
-    # demo_base.model_val_summary('hmt_bp_cube_f_p01-[yolov8x]3', csv_path=csv_path)
-
-    # demo_base.model_val_summary('hmt_rgb-[yolov8x]2', csv_path=csv_path)
-    # demo_base.model_val_summary('hmt_rgb-[yolov8x]3', csv_path=csv_path)
-    # demo_base.model_val_summary('hmt_rgb-[yolov8x]4', csv_path=csv_path)
-
-    # demo_base.model_val_summary('hmt_rgb_f_p01-[yolov8x]', csv_path=csv_path)
-    # demo_base.model_val_summary('hmt_rgb_f_p01-[yolov8x]2', csv_path=csv_path)
-
-    # demo_base.model_val_summary('hmt_t-[yolov8x]2', csv_path=csv_path)
-    # demo_base.model_val_summary('hmt_t-[yolov8x]3', csv_path=csv_path)
-    # demo_base.model_val_summary('hmt_t-[yolov8x]4', csv_path=csv_path)
-
-    # demo_base.model_val_summary('hmt_t_f_p01-[yolov8x]', csv_path=csv_path)
-    # demo_base.model_val_summary('hmt_t_f_p01-[yolov8x]2', csv_path=csv_path)
-
-    # demo_base.model_val_summary('hmt_bp_cube-[yolov8x]', csv_path=csv_path)
-    # demo_base.model_val_summary('hmt_bp_cube-[yolov9e]', csv_path=csv_path)
-    # demo_base.model_val_summary('hmt_bp_cube-[yolov10x]', csv_path=csv_path)
-    # demo_base.model_val_summary('hmt_bp_cube-[yolo11x]', csv_path=csv_path)
-    # demo_base.model_val_summary('hmt_bp_cube-[yolo12x]', csv_path=csv_path)
-    # demo_base.model_val_summary('hmt_bp_cube-[yolo26x]', csv_path=csv_path)
-
-    # demo_base.model_val_summary('hmt_t-[yolov8x]', csv_path=csv_path)
-    # demo_base.model_val_summary('hmt_t-[yolov9e]', csv_path=csv_path)
-    # demo_base.model_val_summary('hmt_t-[yolov10x]', csv_path=csv_path)
-    # demo_base.model_val_summary('hmt_t-[yolo11x]', csv_path=csv_path)
-    # demo_base.model_val_summary('hmt_t-[yolo12x]', csv_path=csv_path)
-    # demo_base.model_val_summary('hmt_t-[yolo26x]', csv_path=csv_path)
-
-    # demo_base.model_val_summary('hmt_t_rgbt-[yolov8x]', csv_path=csv_path)
-    # demo_base.model_val_summary('hmt_t_rgbt-[yolov9e]', csv_path=csv_path)
-    # demo_base.model_val_summary('hmt_t_rgbt-[yolov10x]', csv_path=csv_path)
-    # demo_base.model_val_summary('hmt_t_rgbt-[yolo11x]', csv_path=csv_path)
-    # demo_base.model_val_summary('hmt_t_rgbt-[yolo12x]', csv_path=csv_path)
-    # demo_base.model_val_summary('hmt_t_rgbt-[yolo26x]', csv_path=csv_path)
-
-    # demo_base.model_val_summary('hmt_rgb-[yolov8x]', csv_path=csv_path)
-    # demo_base.model_val_summary('hmt_rgb-[yolov9e]', csv_path=csv_path)
-    # demo_base.model_val_summary('hmt_rgb-[yolov10x]', csv_path=csv_path)
-    # demo_base.model_val_summary('hmt_rgb-[yolo11x]', csv_path=csv_path)
-    # demo_base.model_val_summary('hmt_rgb-[yolo12x]', csv_path=csv_path)
-    # demo_base.model_val_summary('hmt_rgb-[yolo26x]', csv_path=csv_path)
-
-    # demo_base.model_val_summary('hmt_rgb_rgbt-[yolov8x]', csv_path=csv_path)
-    # demo_base.model_val_summary('hmt_rgb_rgbt-[yolov9e]', csv_path=csv_path)
-    # demo_base.model_val_summary('hmt_rgb_rgbt-[yolov10x]', csv_path=csv_path)
-    # demo_base.model_val_summary('hmt_rgb_rgbt-[yolo11x]', csv_path=csv_path)
-    # demo_base.model_val_summary('hmt_rgb_rgbt-[yolo12x]', csv_path=csv_path)
-    # demo_base.model_val_summary('hmt_rgb_rgbt-[yolo26x]', csv_path=csv_path)
+    demo_base.yolo8("yolov8x.yaml", auto_optim=False, name=NAME, data="hmt_rgb.yaml", cls_pw=0.5)
--- demo_det_hmt.py
+++ demo_det_hmt.py
@@ -6,17 +6,196 @@ import demo_base
 
 os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
 demo_base.TASK = "detect"
-demo_base.EPOCHS = 300
+demo_base.EPOCHS = 200
 demo_base.IMGSZ = 640
 demo_base.DEVICE = torch.device("cuda:1")
 demo_base.BATCH_SIZE = 16
-demo_base.CONF_VAL = 0.01
+demo_base.CONF_VAL = 0.001
 
 if __name__ == "__main__":
     NAME = None
 
     # demo_base.model_val("hmt_t-[yolov8x]-8")
-
     # demo_base.model_val("hmt_rgb_merge-[yolov8x]-4")
+    # demo_base.model_val("hmt_bp_cube-[yolov8x]4")
 
-    # demo_base.model_val_summary("hmt_bp_cube-[yolov8x]4")
+    # demo_base.model_val("hmt_t-[yolov8x]-8", save_txt=True, save_conf=True)
+    # demo_base.model_predict("hmt_t-[yolov8x]-8", r"/localnvme/data/bdd_hmt/sua_t/images", save_txt=True, save_conf=True)
+
+    # demo_base.yolo8("yolov8x.yaml", auto_optim=False, name=NAME, data="hmt_t.yaml")
+    # demo_base.yolo9("yolov9e.yaml", auto_optim=False, name=NAME, data="hmt_t.yaml")
+    # demo_base.yolo10("yolov10x.yaml", auto_optim=False, name=NAME, data="hmt_t.yaml")
+    # demo_base.yolo11("yolo11x.yaml", auto_optim=False, name=NAME, data="hmt_t.yaml")
+    # demo_base.yolo12("yolo12x.yaml", auto_optim=False, name=NAME, data="hmt_t.yaml")
+    # demo_base.yolo26("yolo26x.yaml", auto_optim=False, name=NAME, data="hmt_t.yaml")
+
+    # demo_base.yolo8("yolov8x.yaml", auto_optim=False, name=NAME, data="hmt_t.yaml", cls_pw=0.5)
+    # demo_base.yolo9("yolov9e.yaml", auto_optim=False, name=NAME, data="hmt_t.yaml", cls_pw=0.5)
+    # demo_base.yolo10("yolov10x.yaml", auto_optim=False, name=NAME, data="hmt_t.yaml", cls_pw=0.5)
+    # demo_base.yolo11("yolo11x.yaml", auto_optim=False, name=NAME, data="hmt_t.yaml", cls_pw=0.5)
+    # demo_base.yolo12("yolo12x.yaml", auto_optim=False, name=NAME, data="hmt_t.yaml", cls_pw=0.5)
+    # demo_base.yolo26("yolo26x.yaml", auto_optim=False, name=NAME, data="hmt_t.yaml", cls_pw=0.5)
+
+    # demo_base.yolo8("yolov8x.yaml", auto_optim=False, name=NAME, data="hmt_rgb.yaml")
+    # demo_base.yolo9("yolov9e.yaml", auto_optim=False, name=NAME, data="hmt_rgb.yaml")
+    # demo_base.yolo10("yolov10x.yaml", auto_optim=False, name=NAME, data="hmt_rgb.yaml")
+    # demo_base.yolo11("yolo11x.yaml", auto_optim=False, name=NAME, data="hmt_rgb.yaml")
+    # demo_base.yolo12("yolo12x.yaml", auto_optim=False, name=NAME, data="hmt_rgb.yaml")
+    # demo_base.yolo26("yolo26x.yaml", auto_optim=False, name=NAME, data="hmt_rgb.yaml")
+
+    # demo_base.yolo8("yolov8x.yaml", auto_optim=False, name=NAME, data="hmt_rgb.yaml", cls_pw=0.5)
+    # demo_base.yolo9("yolov9e.yaml", auto_optim=False, name=NAME, data="hmt_rgb.yaml", cls_pw=0.5)
+    # demo_base.yolo10("yolov10x.yaml", auto_optim=False, name=NAME, data="hmt_rgb.yaml", cls_pw=0.5)
+    # demo_base.yolo11("yolo11x.yaml", auto_optim=False, name=NAME, data="hmt_rgb.yaml", cls_pw=0.5)
+    # demo_base.yolo12("yolo12x.yaml", auto_optim=False, name=NAME, data="hmt_rgb.yaml", cls_pw=0.5)
+    # demo_base.yolo26("yolo26x.yaml", auto_optim=False, name=NAME, data="hmt_rgb.yaml", cls_pw=0.5)
+
+    # csv_path = r"runs/summary_hmt_t.csv"
+    # demo_base.model_val_summary(
+    #     [
+    #         r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_t-[yolov8x]/weights/best.pt",
+    #         r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_t-[yolov9e]/weights/best.pt",
+    #         r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_t-[yolov10x]/weights/best.pt",
+    #         r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_t-[yolo11x]/weights/best.pt",
+    #         r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_t-[yolo12x]-2/weights/best.pt",
+    #         r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_t-[yolo26x]/weights/best.pt",
+    #         r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_t-[yolov8x]-3/weights/best.pt",
+    #         r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_t-[yolov9e]-2/weights/best.pt",
+    #         r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_t-[yolov10x]-2/weights/best.pt",
+    #         r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_t-[yolo11x]-2/weights/best.pt",
+    #         r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_t-[yolo12x]-3/weights/best.pt",
+    #         r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_t-[yolo26x]-2/weights/best.pt",
+    #     ],
+    #     csv_path=csv_path,
+    #     weight_name=False,
+    # )
+
+    # csv_path = r"runs/summary_hmt_rgb.csv"
+    # demo_base.model_val_summary(
+    #     [
+    #         r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/debug-5/weights/best.pt",
+    #         r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/debug-6/weights/best.pt",
+    #         r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/debug-7/weights/best.pt",
+    #         r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/debug-8/weights/best.pt",
+    #         r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/debug-9/weights/best.pt",
+    #         r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/debug-10/weights/best.pt",
+    #         r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/debug-11/weights/best.pt",
+    #         r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/debug-12/weights/best.pt",
+    #         r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/debug-13/weights/best.pt",
+    #         r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/debug-14/weights/best.pt",
+    #         r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/debug-15/weights/best.pt",
+    #         r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/debug-16/weights/best.pt",
+    #     ],
+    #     csv_path=csv_path,
+    #     weight_name=False,
+    # )
+
+    # demo_base.model_val(
+    #     r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/debug-11/weights/best.pt",
+    #     save_txt=True,
+    #     save_conf=True,
+    #     weight_name=False,
+    # )
+    # demo_base.model_predict(
+    #     r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/debug-11/weights/best.pt",
+    #     r"/localnvme/data/bdd_hmt/sua_rgb/images",
+    #     save_txt=True,
+    #     save_conf=True,
+    #     weight_name=False,
+    #     conf=0.001,
+    # )
+
+    # demo_base.yolo8("yolov8x.yaml", auto_optim=False, name=NAME, data="hmt_rgb.yaml")
+    # demo_base.yolo8("yolov8x.yaml", auto_optim=False, name=NAME, data="hmt_rgb.yaml", cls_pw=0.5)
+    # demo_base.model_val(
+    #     "/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_rgb-[yolov8x]/weights/best.pt",
+    #     weight_name=False,
+    #     save_txt=True,
+    #     save_conf=True,
+    # )
+    # demo_base.model_val(
+    #     "/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_rgb-[yolov8x]-2/weights/best.pt",
+    #     weight_name=False,
+    #     save_txt=True,
+    #     save_conf=True,
+    # )
+
+    # demo_base.model_predict(
+    #     r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_rgb-[yolov8x]/weights/best.pt",
+    #     r"/localnvme/data/bdd_hmt/sua_rgb/images",
+    #     save_txt=True,
+    #     save_conf=True,
+    #     weight_name=False,
+    #     conf=0.001,
+    # )
+    # demo_base.yolo8("yolov8x.yaml", auto_optim=False, name=NAME, data="hmt_rgb.yaml")
+    # demo_base.yolo8("yolov8x.yaml", auto_optim=False, name=NAME, data="hmt_rgb.yaml", cls_pw=0.5)
+    # demo_base.model_val(
+    #     "/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_rgb-[yolov8x]-7/weights/best.pt",
+    #     weight_name=False,
+    #     save_txt=True,
+    #     save_conf=True,
+    # )
+    # demo_base.model_val(
+    #     "/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_rgb-[yolov8x]-8/weights/best.pt",
+    #     weight_name=False,
+    #     save_txt=True,
+    #     save_conf=True,
+    # )
+    # demo_base.model_predict(
+    #     r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_rgb-[yolov8x]-8/weights/best.pt",
+    #     r"/localnvme/data/bdd_hmt/sua_rgb/images",
+    #     save_txt=True,
+    #     save_conf=True,
+    #     weight_name=False,
+    #     conf=0.001,
+    # )
+    # demo_base.yolo8("yolov8x.yaml", auto_optim=False, name=NAME, data="hmt_rgb.yaml")
+    # demo_base.yolo8("yolov8x.yaml", auto_optim=False, name=NAME, data="hmt_rgb.yaml", cls_pw=0.5)
+    # demo_base.yolo8("yolov8x.yaml", auto_optim=False, name=NAME, data="hmt_rgb_update.yaml")
+    # demo_base.yolo8("yolov8x.yaml", auto_optim=False, name=NAME, data="hmt_rgb_update.yaml", cls_pw=0.5)
+    # demo_base.model_val(
+    #     "/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_rgb-[yolov8x]-9/weights/best.pt",
+    #     weight_name=False,
+    #     save_txt=True,
+    #     save_conf=True,
+    # )
+    # demo_base.model_val(
+    #     "/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_rgb-[yolov8x]-11/weights/best.pt",
+    #     weight_name=False,
+    #     save_txt=True,
+    #     save_conf=True,
+    # )
+
+    # demo_base.model_val("hmt_bp_cube-[yolov8x]4", save_txt=True, save_conf=True)
+    # demo_base.model_predict(
+    #     r"hmt_bp_cube-[yolov8x]4",
+    #     r"/localnvme/data/bdd_hmt/bp_cube/images",
+    #     save_txt=True,
+    #     save_conf=True,
+    #     conf=0.001,
+    # )
+    # demo_base.yolo8("yolov8x.yaml", auto_optim=False, name=NAME, data="hmt_bp_cube.yaml")
+    # demo_base.yolo8("yolov8x.yaml", auto_optim=False, name=NAME, data="hmt_bp_cube.yaml", cls_pw=0.5)
+
+    # demo_base.model_val(
+    #     "/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_bp_cube-[yolov8x]/weights/best.pt",
+    #     weight_name=False,
+    #     save_txt=True,
+    #     save_conf=True,
+    # )
+    # demo_base.model_val(
+    #     "/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_bp_cube-[yolov8x]-2/weights/best.pt",
+    #     weight_name=False,
+    #     save_txt=True,
+    #     save_conf=True,
+    # )
+
+    # demo_base.model_predict(
+    #     r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_bp_cube-[yolov8x]/weights/best.pt",
+    #     r"/localnvme/data/bdd_hmt/bp_cube/images",
+    #     weight_name=False,
+    #     save_txt=True,
+    #     save_conf=True,
+    #     conf=0.001,
+    # )
