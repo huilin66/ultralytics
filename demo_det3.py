7bf27625ea7fa5b6894fc7a92450bd186fc1dd8d
huilin66 <23039356r@connect.polyu.hk>
2026-08-27 01:38:00 +0800

    Update demo_det_hmt.py to adjust epochs and enhance yolo8 parameters; add hmt_t_update_v6 dataset configuration

--- demo_det3.py
+++ demo_det3.py
@@ -36,30 +36,38 @@ if __name__ == "__main__":
     #     save_txt=True,
     #     save_conf=True,
     # )
-    demo_base.model_predict(
-        [
-            "tf_defect_3-[yolov8x]",
-        ],
-        r"/scrinvme/huilin/traffic_sign/defect/detection/data_det_3_damaged_traffic_signs/images",
-        save_txt=True,
-        save_conf=True,
-    )
-
-    # demo_base.model_val(
+    # demo_base.model_predict(
     #     [
-    #         # r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_t_update_v2-[yolov8x]/weights/best.pt",
-    #         # r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_t_update_v3-[yolov8x]/weights/best.pt",
-    #         # r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_t_update_v3-[yolov8x]-from-v2-7/weights/best.pt",
-    #         # r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_t_update_v3-[yolov8x]-from-v2-8/weights/best.pt",
-    #         # r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_t_update_v3-[yolov8x]-3/weights/best.pt",
-    #         # r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_t_update_v3-[yolov8x]-4/weights/best.pt",
-    #         r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_t_update_v3-[yolov8x]-8/weights/best.pt",
-    #         r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_t_update_v4-[yolov8x]-2/weights/best.pt",
+    #         # "tf_defect_3-[yolov8x]",
+    #         "f_defect_1-[yolov8x]-4",
     #     ],
-    #     weight_name=False,
-    #     # save_txt=True,
-    #     # save_conf=True,
+    #     r"/scrinvme/huilin/traffic_sign/defect/detection/data_seg_1_damaged-guardrails/images",
+    #     save_txt=True,
+    #     save_conf=True,
     # )
+
+    # demo_base.model_track("tf_det_1-[yolov9e]", img_dir=r"/scrinvme/huilin/tp/front")
+
+    demo_base.model_val(
+        [
+            # r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_t_update_v2-[yolov8x]/weights/best.pt",
+            # r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_t_update_v3-[yolov8x]/weights/best.pt",
+            # r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_t_update_v3-[yolov8x]-from-v2-7/weights/best.pt",
+            # r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_t_update_v3-[yolov8x]-from-v2-8/weights/best.pt",
+            # r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_t_update_v3-[yolov8x]-3/weights/best.pt",
+            # r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_t_update_v3-[yolov8x]-4/weights/best.pt",
+            # r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_t_update_v3-[yolov8x]-8/weights/best.pt",
+            # r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_t_update_v4-[yolov8x]-2/weights/best.pt",
+            # r"hmt_t_update_v6-[yolov8x]-5",
+            # r"hmt_t_update_v6-[yolov8x]-6",
+            # r"hmt_t_update_v6-[yolov8x]-7",
+            # r"hmt_t_update_v6-[yolov8x]-8",
+            r"hmt_t_update_v6-[yolov8x]-10",
+        ],
+        # weight_name=False,
+        # save_txt=True,
+        # save_conf=True,
+    )
     # demo_base.model_predict(
     #     [
     #         r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_t_update_v3-[yolov8x]-7/weights/best.pt",
@@ -67,6 +75,19 @@ if __name__ == "__main__":
     #     ],
     #     r"/localnvme/data/bdd_hmt/hmt_t_update_v3/images",
     #     weight_name=False,
+    #     save_txt=True,
+    #     save_conf=True,
+    # )
+    # demo_base.model_predict(
+    #     [
+    #         # r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_t_update_v4-[yolov8x]-2/weights/best.pt",
+    #         # r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_t_update_v4-[yolov8x]-3/weights/best.pt",
+    #         # r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect/hmt_t_update_v5-[yolov8x]/weights/best.pt",
+    #         # r"hmt_t_update_v6-[yolov8x]-5",
+    #         r"hmt_t_update_v6-[yolov8x]-10",
+    #     ],
+    #     r"/localnvme/data/bdd_hmt/hmt_t_update_v6/images",
+    #     # weight_name=False,
     #     save_txt=True,
     #     save_conf=True,
     # )
--- demo_det_hmt.py
+++ demo_det_hmt.py
@@ -6,7 +6,7 @@ import demo_base
 
 os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
 demo_base.TASK = "detect"
-demo_base.EPOCHS = 300
+demo_base.EPOCHS = 100
 demo_base.IMGSZ = 640
 demo_base.DEVICE = torch.device("cuda:1")
 demo_base.BATCH_SIZE = 16
@@ -291,16 +291,20 @@ if __name__ == "__main__":
         weight_path="yolov8x.pt",
         load_as_model=False,
         auto_optim=False,
-        name="hmt_t_update_v4-[yolov8x]",
-        data="hmt_t_update_v4.yaml",
+        name="hmt_t_update_v6-[yolov8x]",
+        data="hmt_t_update_v6.yaml",
         cls_pw=0.5,
+        hsv_h=0.0,
+        hsv_s=0.0,
+        hsv_v=0.0,
+        mosaic=0.25,
     )
-    demo_base.yolo8(
-        "yolov8x.yaml",
-        weight_path="yolov8x.pt",
-        load_as_model=False,
-        auto_optim=False,
-        name="hmt_t_update_v5-[yolov8x]",
-        data="hmt_t_update_v5.yaml",
-        cls_pw=0.5,
-    )
+    # demo_base.yolo8(
+    #     "yolov8x.yaml",
+    #     weight_path="yolov8x.pt",
+    #     load_as_model=False,
+    #     auto_optim=False,
+    #     name="hmt_t_update_v5-[yolov8x]",
+    #     data="hmt_t_update_v5.yaml",
+    #     cls_pw=0.5,
+    # )
