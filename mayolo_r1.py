616adf9383b09180bb34868c30049b400036f010
huilin66 <23039356r@connect.polyu.hk>
2026-09-07 15:05:54 +0800

    feat: Update experiment directory and parameters for model training

--- mayolo_r1.py
+++ mayolo_r1.py
@@ -416,4 +416,4 @@ if __name__ == "__main__":
     # model_val(r"runs/mdetect/myolo10x_stage2/weights/best.pt")
     # model_val_dir(r"runs/mdetect")  # validates best.pt & last.pt under every exp dir
     # model_val_dir(r"runs/mdetect", save_txt=r"runs/mdetect/mdetect_stats.csv")  # val+test in one row, CSV
-    model_val_dir(r"runs/experiments/E1_w4")
+    model_val_dir(r"runs/experiments/E1_w4_test")
