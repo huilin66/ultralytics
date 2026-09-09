1d23cd33769e3a077d9ddfb308b81dcc1c5fa11c
huilin66 <23039356r@connect.polyu.hk>
2026-09-09 17:53:14 +0800

    feat: Update stage2-sweep command and add w4 training parameters for consistency

--- run.sh
+++ run.sh
@@ -25,11 +25,24 @@ DATA=ultralytics/cfg/mayolo_r1/mayolo_v3.yaml
 #   --project runs/experiments/E0_stage1_sweep
 
 
-python scripts/train_mdet_experiments.py stage2-sweep \
-  --data "$DATA" \
+# python scripts/train_mdet_experiments.py stage2-sweep \
+#   --data "$DATA" \
+#   --model ultralytics/cfg/models/experiments/yolov10x-mdetect.yaml \
+#   --stage1-checkpoint runs/experiments/E0_stage1_sweep/E0_stage1_stage1_100_w4_0p5_seed_0/weights/best.pt \
+#   --stage1-epochs 100 \
+#   --stage2-values 50 100 150 200 \
+#   --w4 0.5 \
+#   --project runs/experiments/E0_stage2_sweep
+
+
+python scripts/train_mdet_experiments.py w4 \
+  --data ultralytics/cfg/mayolo_r1/mayolo_v3.yaml \
   --model ultralytics/cfg/models/experiments/yolov10x-mdetect.yaml \
-  --stage1-checkpoint runs/experiments/E0_stage1_sweep/E0_stage1_stage1_100_w4_0p5_seed_0/weights/best.pt \
+  --pretrain yolov10x.pt \
+  --w4-values 0.25 0.5 1.0 \
   --stage1-epochs 100 \
-  --stage2-values 50 100 150 200 \
-  --w4 0.5 \
-  --project runs/experiments/E0_stage2_sweep
+  --stage2-epochs 100 \
+  --hsv-h 0 \
+  --hsv-s 0.2 \
+  --hsv-v 0.2 \
+  --project runs/experiments/E1_w4
