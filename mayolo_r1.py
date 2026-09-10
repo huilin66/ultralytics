c560eb498b7054a0f99ab89c12d25f0a405bcf7b
huilin66 <23039356r@connect.polyu.hk>
2026-09-10 17:18:53 +0800

    feat: Update model validation directory for E2.1 GIA-v2 position experiments

--- mayolo_r1.py
+++ mayolo_r1.py
@@ -474,4 +474,4 @@ if __name__ == "__main__":
     # model_val(r"runs/mdetect/myolo10x_stage2/weights/best.pt")
     # model_val_dir(r"runs/mdetect")  # validates best.pt & last.pt under every exp dir
     # model_val_dir(r"runs/mdetect", save_txt=r"runs/mdetect/mdetect_stats.csv")  # val+test in one row, CSV
-    model_val_dir(r"runs/experiments/E2_1_GIA_position")
+    model_val_dir(r"runs/experiments/E2_1_GIA_v2_position")
